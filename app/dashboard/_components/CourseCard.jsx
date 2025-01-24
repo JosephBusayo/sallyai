4afdde4bf723f651b7c4c064d3c29f3e58f27eae
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2025-01-24 23:03:13 +0100

    Edited course list page

--- app/dashboard/_components/CourseCard.jsx
+++ app/dashboard/_components/CourseCard.jsx
@@ -19,27 +19,27 @@ function CourseCard({ course, refreshData, displayUser=false }) {
     }
 
     return (
-        <div className='shadow-sm rounded-lg border p-2 md:hover:scale-105 transition-all cursor-pointer mt-4 '>
+        <div className='shadow-sm rounded-lg border p-2 md:hover:scale-105 transition-all cursor-pointer mt-4'>
             <Link href={`/course/${course?.courseId}`}>
-                <Image src={course?.courseBanner} width={300} height={300} className='w-full h-[150px] md:h-[200px] object-cover rounded-lg ' />
+                <Image src={course?.courseBanner} width={300} height={300} className='w-full h-[140px] md:h-[200px] object-cover rounded-lg ' />
             </Link>
 
             <div className='p-2'>
                 {/*install schcn dropdown*/}
-                <h2 className='font-medium text-1xl md:text-lg flex justify-between items-center'>{course?.courseOutput?.course?.name}
+                <h2 className='font-medium text-[14px] md:text-lg flex justify-between items-center'>{course?.courseOutput?.course?.name}
                     {!displayUser && <DropdownOption handleOnDelete={() => handleOnDelete()}>
                         <HiMiniEllipsisVertical />
                     </DropdownOption>}
                 </h2>
-                <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
-                <div className='flex items-center justify-between'>
-                    <h2 className='flex gap-2 items-cente text-primary text-sm rounded-sm'><HiOutlineBookOpen />{course?.courseOutput?.course?.numberOfChapters} 2 Chapters</h2>
+                <p className='text-sm text-gray-400 my-2'>{course?.category}</p>
+                <div className='flex flex-wrap items-center justify-between my-2'>
+                    <h2 className='hidden md:flex gap-2 items-cente text-primary text-sm rounded-sm'><HiOutlineBookOpen />{course?.courseOutput?.course?.numberOfChapters}Chapters</h2>
                     <h2 className='text-sm text-primary p-1 rounded-sm'>{course?.level} </h2>
                 </div>
 
                 <div className='flex gap-2 items-center mt-2'>
-                    <Image src={course?.userProfileImage} width={30} height={30} className='rounded-full'/>
-                    <h2 className='text-sm'>{course?.userName}</h2>
+                    <Image src={course?.userProfileImage} width={20} height={20}  className='rounded-full'/>
+                    <h2 className='text-[11px] md:text-sm'>{course?.userName}</h2>
                 </div>
             </div>
         </div>
