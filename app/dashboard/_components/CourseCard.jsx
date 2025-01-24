ef0639472a0dbdd741737bfa6fb8418fbdde5063
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2025-01-24 13:09:28 +0100

    added explore page

--- app/dashboard/_components/CourseCard.jsx
+++ app/dashboard/_components/CourseCard.jsx
@@ -7,7 +7,7 @@ import { CourseList } from '@/app/configs/schema'
 import Link from 'next/link'
 
 
-function CourseCard({ course, refreshData }) {
+function CourseCard({ course, refreshData, displayUser=false }) {
     const handleOnDelete = async () => {
         const resp = await db.delete(CourseList)
             .where(eq(CourseList.id, course?.id))
@@ -27,14 +27,19 @@ function CourseCard({ course, refreshData }) {
             <div className='p-2'>
                 {/*install schcn dropdown*/}
                 <h2 className='font-medium text-lg flex justify-between items-center'>{course?.courseOutput?.course?.name}
-                    <DropdownOption handleOnDelete={() => handleOnDelete()}>
+                    {!displayUser && <DropdownOption handleOnDelete={() => handleOnDelete()}>
                         <HiMiniEllipsisVertical />
-                    </DropdownOption>
+                    </DropdownOption>}
                 </h2>
                 <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
                 <div className='flex items-center justify-between'>
                     <h2 className='flex gap-2 items-center p-1 bg-gray-600 text-primary text-sm rounded-sm'><HiOutlineBookOpen />{course?.courseOutput?.course?.numberOfChapters} Chapters</h2>
                     <h2 className='text-sm bg-gray-600 text-primary p-1 rounded-sm'>{course?.level} </h2>
+                </div>
+
+                <div className='flex gap-2 items-center mt-2'>
+                    <Image src={course?.userProfileImage} width={30} height={30} className='rounded-full'/>
+                    <h2 className='text-sm'>{course?.userName}</h2>
                 </div>
             </div>
         </div>
