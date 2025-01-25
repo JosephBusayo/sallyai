b00b734d6de29658e019a5613d7ef55ca85d09f8
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2025-01-25 08:24:42 +0100

    fixed hero section button hover effect

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function Hero() {
@@ -18,12 +19,9 @@ function Hero() {
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
-            <a
-              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-accent focus:outline-none focus:ring active:bg-accent sm:w-auto"
-              href="/dashboard"
-            >
-              Get Started
-            </a>
+              <a href="/dashboard" >
+                  <Button >Get started </Button>
+                </a>
           </div>
         </div>
       </div>
